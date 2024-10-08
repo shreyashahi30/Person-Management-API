0ae7ef50f4c1778d4de1ab824ee57a04f14e1dcd
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-08 18:39:05 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,20 @@
+// config/db.js
+
+const mongoose = require('mongoose');
+const mongoConfig = require('./mongoConfig');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoConfig.uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDB;
 

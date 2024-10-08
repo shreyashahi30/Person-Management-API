94fd3ea27570803e7f0bab7c5f75d4fb20d47a14
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-08 18:40:24 +0530

    Update jwtConfig.js

--- config/jwtConfig.js
+++ config/jwtConfig.js
@@ -1,1 +1,9 @@
+// config/jwtConfig.js
+
+const jwtConfig = {
+    secret: process.env.JWT_SECRET || '3f55c8e8c5eb4d5c8e8bc50c4f6e4732a4e14f3cfb5a40c10f6e18f2b3c16b7a', 
+    expiresIn: '1h', // Token expiration time
+};
+
+module.exports = jwtConfig;
 
